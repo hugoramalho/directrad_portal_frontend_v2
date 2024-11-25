310d04527dd9fc7fb76b188f5260c4c7e9376ec7
Hugo Ramalho <ramalho.hg@gmail.com>
2024-11-25 17:26:22 -0300

    wip

--- src/app/apps/to-do-list/menu/options-menu.component.ts
+++ src/app/apps/to-do-list/menu/options-menu.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-basic-menu',
   standalone: true,
   imports: [],
   templateUrl: './options-menu.component.ts',
-  styleUrl: './basic-menu.component.scss'
+  styleUrl: './options-menu.component.scss'
 })
 export class optionsMenuEstudosComponent {
+  @Input() estudoId: string | undefined;
 
+  executarAcao(acao: string) {
+    console.log(`Ação '${acao}' executada no estudo com ID: ${this.estudoId}`);
+  }
+  
 }
