9ba359420e352143a1fd175a42fb9d2ccfc6efda
Hugo Ramalho <ramalho.hg@gmail.com>
2024-11-25 13:55:52 -0300

    wip

---
+++ src/app/apps/to-do-list/menu/options-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './options-menu.component.ts',
+  styleUrl: './basic-menu.component.scss'
+})
+export class optionsMenuEstudosComponent {
+
+}
