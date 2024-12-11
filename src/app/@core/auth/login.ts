46a2bdaaff2f4796b2b2a2c0a25399add3ce04a8
Hugo Ramalho <ramalho.hg@gmail.com>
2024-12-11 17:07:35 -0300

    wip

---
+++ src/app/@core/auth/login.ts
@@ -0,0 +1,11 @@
+/**
+ Created by: Hugo Ramalho <ramalho.hg@gmail.com>
+
+ Created at: 11/12/2024
+ **/
+
+export interface Login {
+    jwt: string;
+    expiraEm: number;
+    usuarioId: number;
+}
---
+++ src/app/@core/auth/user.ts
@@ -0,0 +1,6 @@
+/**
+ Created by: Hugo Ramalho <ramalho.hg@gmail.com>
+
+ Created at: 11/12/2024
+ **/
+
