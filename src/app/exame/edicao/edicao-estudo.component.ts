51ce9e43c075cd521b50ecb5c30586f4ab90850a
Hugo Ramalho <ramalho.hg@gmail.com>
2024-12-17 14:12:27 -0300

    wip

--- src/app/exame/edicao/edicao-estudo.component.ts
+++ src/app/exame/edicao/edicao-estudo.component.ts
@@ -43,7 +43,7 @@
 //
 // }
 
-import { Component, Inject } from '@angular/core';
+import {Component, Inject} from '@angular/core';
 import {MatDialogRef, MAT_DIALOG_DATA, MatDialogContent, MatDialogActions} from '@angular/material/dialog';
 import {EstudoRepository} from "../exame.service";
 import {
@@ -63,6 +63,7 @@ import {FormsModule} from "@angular/forms";
 import {TagDicom} from "../tag-dicom";
 import {Estudo} from "../exame";
 import {MatProgressSpinner} from "@angular/material/progress-spinner";
+import {MatTooltip} from "@angular/material/tooltip";
 
 @Component({
     standalone: true,
@@ -88,7 +89,8 @@ import {MatProgressSpinner} from "@angular/material/progress-spinner";
         MatRowDef,
         MatDialogActions,
         MatButton,
-        MatProgressSpinner
+        MatProgressSpinner,
+        MatTooltip
     ]
 })
 export class EditarEstudoModalComponent {
@@ -137,10 +139,8 @@ export class EditarEstudoModalComponent {
         PatientName: "00100010",
         PatientBirthDate: "00100030",
         PatientSex: "00100040",
-        PatientAge: "00101010",
         PatientSize: "00101020",
         PatientWeight: "00101030",
-        PatientAddress: "00101040",
         PatientTelephoneNumbers: "00102154",
         PatientEmail: "00100032",
         PatientInsurancePlan: "00101081",
@@ -152,12 +152,47 @@ export class EditarEstudoModalComponent {
         NumberOfPatientRelatedStudies: "00201200",
     };
 
+    dicomEditableTagMap: Record<string, string> = {
+        StudyDate: "00080020",
+        StudyModality: "00080061",
+        ReferringPhysicianName: "00080090",
+        StudyDescription: "00081030",
+        StudyPriorityID: "0032000C",
+        ReasonForStudy: "00321030",
+        RequestingService: "00321033",
+        StudyComments: "00324000",
+        AdmissionID: "00380010",
+        RequestedProcedureID: "00401001",
+        ProcedureCode: "00081032",
+        RequestingPhysicianPhone: "00080094",
+        PatientTransportArrangements: "00401004",
+        PatientNameIdeographic: "00100010_Ideographic",
+        PatientNamePhonetic: "00100010_Phonetic",
+        OtherPatientNames: "00101001",
+        PatientComments: "00104000",
+        ReasonForTheAttributeModification: "04000565",
+        PatientID: "00100020",
+        PatientName: "00100010",
+        PatientBirthDate: "00100030",
+        PatientSex: "00100040",
+        PatientSize: "00101020",
+        PatientWeight: "00101030",
+        PatientTelephoneNumbers: "00102154",
+        PatientEmail: "00100032",
+        PatientInsurancePlan: "00101081",
+        EthnicGroup: "00102160",
+        Occupation: "00102180",
+        MedicalAlerts: "00102000",
+        Allergies: "00102110",
+        AdditionalPatientHistory: "001021B0",
+    };
 
     constructor(
         public dialogRef: MatDialogRef<EditarEstudoModalComponent>,
         @Inject(MAT_DIALOG_DATA) public data: { uid: string },
         private estudoService: EstudoRepository
-    ) {}
+    ) {
+    }
 
     ngOnInit(): void {
         this.loadEstudo();
@@ -167,20 +202,23 @@ export class EditarEstudoModalComponent {
         this.estudoService.getEstudo(this.data.uid).subscribe({
             next: (estudos) => {
                 const estudo: Estudo = estudos[0];
-                this.estudoTags = Object.keys(estudo).map((key) => {
-                    const typedKey = key as keyof Estudo; // Garantimos que a key é uma chave de Estudo
-                    return {
-                        tag: this.dicomTagMap[key] || 'N/A',
-                        name: key,
-                        value: estudo[typedKey] || '', // Usamos o typedKey aqui
-                        editable: true, // Define se a tag é editável
-                        isEditing: false, // Controla o estado de edição
-                    };
-                });
+                this.estudoTags = Object.keys(estudo)
+                    .filter((key) => this.dicomTagMap.hasOwnProperty(key))
+                    .map((key) => {
+                        const typedKey = key as keyof Estudo;
+                        const editable = this.dicomEditableTagMap.hasOwnProperty(key);
+                        return {
+                            tag: this.dicomTagMap[key] || 'N/A',
+                            name: key,
+                            value: estudo[typedKey] || '',
+                            isEditable: editable,
+                            isEditing: false,
+                        };
+                    });
                 this.isLoading = false;
             },
             error: (err) => {
-                console.error('Erro ao carregar o estudo:', err);
+                this.isLoading = false;
             },
         });
     }
@@ -201,7 +239,7 @@ export class EditarEstudoModalComponent {
     onSave(): void {
         const updatedTags = this.estudoTags
             .filter((tag) => tag.editable)
-            .map(({ name, value }) => ({ name, value }));
+            .map(({name, value}) => ({name, value}));
 
         this.dialogRef.close(updatedTags); // Retorna as alterações
     }
