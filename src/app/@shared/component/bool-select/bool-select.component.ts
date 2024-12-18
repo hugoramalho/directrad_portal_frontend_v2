5fbf7cfd09c350b8886cf8f5ce3bf72059c7203b
Hugo Ramalho <ramalho.hg@gmail.com>
2024-12-18 17:30:14 -0300

    wip bool select

---
+++ src/app/@shared/component/bool-select/bool-select.component.ts
@@ -0,0 +1,60 @@
+import { Component, forwardRef, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
+@Component({
+    selector: 'app-bool-select',
+    standalone: true,
+    imports: [CommonModule, MatFormFieldModule, MatSelectModule],
+    templateUrl: './bool-select.component.html',
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => TrueFalseSelectComponent),
+            multi: true
+        }
+    ],
+    styleUrls: ['./bool-select.component.scss']
+})
+export class TrueFalseSelectComponent implements ControlValueAccessor {
+    @Input() useMaterial: boolean = false; // Define o uso do Material Design
+    @Input() label: string = 'Escolha'; // Rótulo do campo
+
+    value: boolean | null = null;
+
+    // Opções do select
+    options = [
+        { value: true, label: 'Positivo' },
+        { value: false, label: 'Negativo' }
+    ];
+
+    // Callbacks para o ControlValueAccessor
+    onChange: (value: boolean | null) => void = () => {};
+    onTouched: () => void = () => {};
+
+    onValueChange(value: boolean): void {
+        this.value = value;
+        this.onChange(value);
+        this.onTouched();
+    }
+
+    onNativeValueChange(event: Event): void {
+        const target = event.target as HTMLSelectElement;
+        const value = target.value === 'true'; // Converte para boolean
+        this.onValueChange(value);
+    }
+
+    writeValue(value: boolean | null): void {
+        this.value = value;
+    }
+
+    registerOnChange(fn: (value: boolean | null) => void): void {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: () => void): void {
+        this.onTouched = fn;
+    }
+}
