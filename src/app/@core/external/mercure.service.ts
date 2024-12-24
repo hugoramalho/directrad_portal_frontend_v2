d0b62a9ac12ec04c78ad8bc64d15b94987ff4abb
Hugo Ramalho <ramalho.hg@gmail.com>
2024-12-24 09:59:38 -0300

    feat: mercure

---
+++ src/app/@core/external/mercure.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class MercureService {
+    private mercureUrl = 'http://localhost/.well-known/mercure';
+
+    subscribeToTopic(topic: string): Observable<any> {
+        return new Observable((observer) => {
+            const eventSource = new EventSource(
+                `${this.mercureUrl}?topic=${encodeURIComponent(topic)}`
+            );
+
+            eventSource.onmessage = (event) => {
+                observer.next(JSON.parse(event.data));
+            };
+
+            eventSource.onerror = (error) => {
+                observer.error(error);
+            };
+
+            return () => eventSource.close();
+        });
+    }
+}
