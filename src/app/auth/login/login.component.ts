1011b1337108e6274ee18d8036ae1e4cd426493d
Hugo Ramalho <ramalho.hg@gmail.com>
2025-02-03 08:12:27 -0300

    wip

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -14,16 +14,17 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { NgIf } from '@angular/common';
 import { Router } from '@angular/router';
 import { CustomizerSettingsService } from '../../customizer-settings/customizer-settings.service';
-import {AuthService} from "../../@core/auth/auth.service";
+import { AuthService } from "../../@core/auth/auth.service";
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
-    selector: 'app-sign-in',
+    selector: 'app-login-component',
     standalone: true,
     imports: [RouterLink, MatFormFieldModule, MatInputModule, MatButtonModule, MatCheckboxModule, ReactiveFormsModule, NgIf],
-    templateUrl: './sign-in.component.html',
-    styleUrl: './sign-in.component.scss'
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.scss'
 })
-export class SignInComponent {
+export class LoginComponent {
 
     // isToggled
     isToggled = false;
@@ -33,6 +34,7 @@ export class SignInComponent {
         private router: Router,
         public themeService: CustomizerSettingsService,
         private authService: AuthService,
+        private snackBar: MatSnackBar
     ) {
         this.authForm = this.fb.group({
             email: ['', [Validators.required, Validators.email]],
@@ -46,21 +48,38 @@ export class SignInComponent {
     hide = true;
     authForm: FormGroup;
     onSubmit() {
+        // this.snackBar.dismiss();
+        this.snackBar.open('Falha na autenticação', 'Fechar', {
+            duration: 500,
+            horizontalPosition: 'right',
+            verticalPosition: 'bottom',
+            panelClass: ['success-snackbar'] // Classe CSS personalizada
+
+        }).afterDismissed().subscribe(() => {
+            console.log('Snackbar fechado automaticamente.');
+        });;
         if (this.authForm.valid) {
-            this.authService.login(this.authForm.get('email')?.value, this.authForm.get('password')?.value)
+            this.authService.login(this.authForm.get('username')?.value, this.authForm.get('password')?.value)
                 .subscribe({
                     next: (response) => {
-                        this.authService.handleAuthentication(response.data);
-                        this.router.navigate(['/financeiro/extratos']);
+                        if (this.authService.handleAuthentication(response)) {
+                            // Redireciona para "/estudos" após login bem-sucedido
+                            this.router.navigate(['/estudos']);
+                        }
                     },
                     error: (error) => {
-                        console.error('Erro no login', error)
+                        this.snackBar.open('Operação realizada com sucesso!', 'Fechar', {
+                            duration: 3000, // tempo em milissegundos para fechar automaticamente
+                            horizontalPosition: 'center', // 'start' | 'center' | 'end' | 'left' | 'right'
+                            verticalPosition: 'top',      // 'top' | 'bottom'
+                            panelClass: ['success-snackbar'] // Classe CSS personalizada
+
+                        });
+                        console.error('Erro no login', error);
                     }
                 });
-
-            this.router.navigate(['/']);
         } else {
-            console.log('Form is invalid. Please check the fields.');
+            console.log('Formulário inválido. Verifique os campos.');
         }
     }
 
